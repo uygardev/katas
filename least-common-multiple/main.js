f9df277e005dc849aad768ed3c30ea5f23e72685
uygar aydin <hi@uygaraydin.com>
2021-12-26 03:01:55 +0300

    least-common-multiple

---
+++ least-common-multiple/main.js
@@ -0,0 +1,30 @@
+// * RULE: a*b = gcd(a,b)*lcm(a,b)
+// * https://www.onlinemathlearning.com/image-files/euclidean-algorithm.png
+// * https://www.bubblyprimes.com/wp-content/uploads/2019/07/What-is-a-GCF.jpg
+
+// var gcd = function (x, y) {
+//   let remainder = x % y;
+//   x = y;
+//   if (remainder != 0) y = remainder;
+//   while (remainder != 0) {
+//     remainder = x % y;
+//     x = y;
+//     if (remainder != 0) y = remainder;
+//   }
+
+//   return y
+// }
+
+function gcd(x, y) {
+  var remainder = x % y;
+  [x, y] = [y, remainder];
+  return (remainder != 0) ? gcd(x, y) : x
+}
+
+var lcm = (...numbers) => {
+  return numbers.reduce((x, y) => x * (y / gcd(x, y)));
+}
+
+console.log(lcm(2, 5), 10);
+console.log(lcm(2, 3, 4), 12);
+console.log(lcm(9), 9);
